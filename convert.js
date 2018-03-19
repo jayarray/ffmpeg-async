1d652e54c2c1a786000f8c2f7da7a9f1ed1010f0
Jesus Arevalo <isa90201@gmail.com>
2018-03-18 20:09:14 -0700

    Moved convert function to its own file.

---
+++ convert.js
@@ -0,0 +1,51 @@
+
+let LINUX = require('linux-commands-async.js');
+let LOCAL_COMMAND = LINUX.Command.LOCAL;
+
+//---------------------------------------
+// ERROR
+
+function SrcDestValidator(src) {
+  if (src === undefined)
+    return 'undefined';
+  else if (src == null)
+    return 'null';
+  else if (src == '')
+    return 'empty';
+  else if (src.trim() == '')
+    return 'whitespace';
+  else
+    return null;
+}
+
+//----------------------------------------
+// CONVERT
+
+/**
+ * @returns {Promise} Returns a promise that resolves if successful. Otherwise, it returns an error.
+ */
+function Convert(src, dest) {
+  let srcError = SrcDestValidator(src);
+  if (srcError)
+    return Promise.reject(`Failed to convert: Source is ${srcError}`);
+
+  let destError = SrcDestValidator(dest);
+  if (destError)
+    return Promise.reject(`Failed to convert: Destination is ${destError}`);
+
+  return new Promise((resolve, reject) => {
+    let args = ['-i', src, dest];
+    LOCAL_COMMAND.Execute('ffmpeg', args).then(output => {
+      if (output.stderr) {
+        reject(`Failed to convert: ${output.stderr}`);
+        return;
+      }
+      resolve();
+    }).catch(error => `Failed to convert: ${error}`);
+  });
+}
+
+//--------------------------------
+// EXPORTS
+
+exports.Convert = Convert;
